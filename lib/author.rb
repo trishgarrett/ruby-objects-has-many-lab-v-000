f647d8ba5509b3a78177062a92c26ee6a92330fc
Trisha Garrett <trisha.garrett@gmail.com>
2018-02-02 03:59:59 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,23 @@
+class Author
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(post_title)
+    title = Post.new(post_title)
+    @posts << post
+    title.author = self
+  end
+end
